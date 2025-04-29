c4c06a101a59db50de527bc9eca12bf678261314
Andy025824 <65064485+Andy025824@users.noreply.github.com>
2025-04-29 11:42:45 +0530

    2nd

---
+++ dog.go
@@ -0,0 +1,9 @@
+package doggo
+
+func Bark1() string {
+	return "Woof!----Doggo"
+}
+
+func Bark2() string {
+	return "Woof! Woof! Woof!----Doggo"
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Andy025824/DogGo
+
+go 1.24.1
